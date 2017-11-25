ece48e2e302050a1ed26219193fbdb4a113b6d9a
JaskaranSidhu <32685059+JaskaranSidhu@users.noreply.github.com>
2017-11-25 15:47:05 -0700

    Add files via upload

---
+++ Bodyman.php
@@ -0,0 +1,16 @@
+<?php
+$link = mysqli_connect("localhost", "root", "");
+mysqli_select_db($link, "cpsc471db") or ('Unable to connect to the Database');
+
+?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			Bodyman Table
+		</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso=8859-1">
+	</head>
