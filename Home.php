e9ac20a98e67c4687176042a3d667a42b772e12b
Lamess Kharfan <lamess19@hotmail.com>
2017-11-27 22:24:03 -0700

    Add files via upload

--- Home.php
+++ Home.php
@@ -1,7 +1,41 @@
 <!DOCTYPE html>
 <html>
-
-<h2> Welcome to the home page </h2>
-
+     <head>
+        <title> Home </title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class = "heading">
+            <img src="https://d2oeydowngaei1.cloudfront.net/resources/front/images/carstar-logo.png">
+            <h1> Welcome </h1>
+        </div>     
+        <div class ="row">
+           <div class = "column">
+                <a href="http://localhost/cpsc471/View_Estimate.php">
+                    <img src="img/manage-estimates.png">
+                </a>
+               <a href="http://localhost/cpsc471/View_Parts.php">
+                   <img src="img/parts.png">
+                </a>
+           </div>
+            <div class = "column">
+                <a href="http://localhost/cpsc471/View_Employees.php">
+                    <img src="img/employee-button.png">
+                </a>
+                <a href="http://localhost/cpsc471/View_Works_On.php">
+                    <img src="img/ro-assignments.png">
+                </a>
+           </div>
+            <div class = "column">
+            <a href="http://localhost/cpsc471/View_Repair_Order.php">
+                <img src="img/manage-ro.png">
+            </a>
+                <!--<a href="http://localhost/cpsc471/View_.php">-->
+                   <img src="img/vehicles.png">
+                <!--</a>-->
+            </div>
+        </div>
+    </body>
 
 </html>
