90c809d1eeb46946ddbe8f8fb15743bc317bed45
Shannon TJ <shannontj@shaw.ca>
2017-11-28 22:27:38 -0700

    Working search

--- View_Estimate.php
+++ View_Estimate.php
@@ -2,65 +2,129 @@
 $link = mysqli_connect("localhost", "root", "", "cpsc471db");
 
 if(!$link) {
-    die('Unable to connect'. mysqli_error($link));
+	die('Unable to connect'. mysqli_error($link));
 }
+
 ?>
+
+
 
 <!DOCTYPE HTML>
 <html>
-    <head>
-            <title>
-                    View Estimate
-            </title>
-            <meta http-equiv="Content-Type" content="text/html; charset=iso=8859-1">
-            <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        
-        <a href="http://localhost/cpsc471/NewEstimate.php">
-            <img src="img/insert-estimate.png">
-        </a>
-        
-        <a href="http://localhost/cpsc471/DeleteEstimate.php">
-            <img src="img/delete-estimate.png">
-        </a>
-        
-        <table width="700" border="1" cellpadding="1" cellspacing="1">
-                <tr>
-                        <th>Est_Num</th>
-                        <th>ID</th>
-                        <th>Plate_Num</th>
-                        <th>Job_Class</th>
-                        <th>Hours</th>
-                        <th>Status</th>
-                        <th>Cost</th>
-                </tr>
+	<head>
+		<title>
+			View Estimate
+		</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso=8859-1">
+	</head>
+	<body>
+		<form action ="View_Estimate.php" method="GET">
+		<input type="text" name="search" placeholder="Enter a value"/>
+		<input type="submit" value="Search"/>
+		</form>
+		<table width="700" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>Est_Num</th>
+				<th>ID</th>
+				<th>Plate_Num</th>
+				<th>Job_Class</th>
+				<th>Hours</th>
+				<th>Status</th>
+				<th>Cost</th>
+			</tr>
 
-                <?php
+			<?php
 
-                $query="SELECT * FROM estimate";
-                $result = mysqli_query($link, $query);
+			//If the user performs a search
+			if(isset($_GET["search"]))
+			{
+				$search = $_GET["search"];
+				$search = mysqli_real_escape_string($link, $search);
+				
+				$query = "SELECT * FROM estimate WHERE Est_Num = $search or ID = $search or Plate_Num = $search 
+				or Hours = $search or Cost = $search";
+				
+				$query2 = "SELECT * FROM estimate WHERE Job_Class = '$search' or Status = '$search'";		
+				
+				//If there are results...
+				if(mysqli_query($link, $query))
+				{
+					$result = mysqli_query($link, $query);	
+					
+					while($estimate=mysqli_fetch_assoc($result)) 
+					{
+						echo "<tr>";
 
-                while($estimate=mysqli_fetch_assoc($result)) {
-                        echo "<tr>";
+						echo "<td>".$estimate['Est_Num']."</td>";
 
-                        echo "<td>".$estimate['Est_Num']."</td>";
+						echo "<td>".$estimate['ID']."</td>";
 
-                        echo "<td>".$estimate['ID']."</td>";
+						echo "<td>".$estimate['Plate_Num']."</td>";
 
-                        echo "<td>".$estimate['Plate_Num']."</td>";
+						echo "<td>".$estimate['Job_Class']."</td>";
 
-                        echo "<td>".$estimate['Job_Class']."</td>";
+						echo "<td>".$estimate['Hours']."</td>";
 
-                        echo "<td>".$estimate['Hours']."</td>";
+						echo "<td>".$estimate['Status']."</td>";
 
-                        echo "<td>".$estimate['Status']."</td>";
+						echo "<td>".$estimate['Cost']."</td>";
+						
+						echo "</tr>";
+					} //End While
+						
+				}
+				else if(mysqli_query($link, $query2))
+				{
+					$result = mysqli_query($link, $query2);
+					
+					while($estimate=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                        echo "<td>".$estimate['Cost']."</td>";
+						echo "<td>".$estimate['Est_Num']."</td>";
 
-                        echo "</tr>";
-                } //End While
-                ?>
-        </table>
-</body>
+						echo "<td>".$estimate['ID']."</td>";
+
+						echo "<td>".$estimate['Plate_Num']."</td>";
+
+						echo "<td>".$estimate['Job_Class']."</td>";
+
+						echo "<td>".$estimate['Hours']."</td>";
+
+						echo "<td>".$estimate['Status']."</td>";
+
+						echo "<td>".$estimate['Cost']."</td>";
+						
+						echo "</tr>";
+					} //End While					
+				}
+			}			
+
+			else
+			{
+				$query="SELECT * FROM estimate";
+				$result = mysqli_query($link, $query);
+					
+				while($estimate=mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+
+					echo "<td>".$estimate['Est_Num']."</td>";
+
+					echo "<td>".$estimate['ID']."</td>";
+
+					echo "<td>".$estimate['Plate_Num']."</td>";
+
+					echo "<td>".$estimate['Job_Class']."</td>";
+
+					echo "<td>".$estimate['Hours']."</td>";
+
+					echo "<td>".$estimate['Status']."</td>";
+
+					echo "<td>".$estimate['Cost']."</td>";
+					
+					echo "</tr>";
+				} //End While
+			}
+			?>
+		</table>
+	</body>
 </html>
