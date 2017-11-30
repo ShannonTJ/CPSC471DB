84abd390e17393bb32ce519eb71822db64414b47
JaskaranSidhu <32685059+JaskaranSidhu@users.noreply.github.com>
2017-11-30 14:02:18 -0700

    Needs Search
    Has Insurance, insured by/with, customer, vehicle.

---
+++ View_CustomerInfo.php
@@ -0,0 +1,187 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", "cpsc471db");
+
+if(!$link) {
+	die('Unable to connect'. mysqli_error($link));
+}
+
+?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			View Repair Order
+		</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso=8859-1">
+	</head>
+	<body>
+		<table width="700" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>Cust_Num</th>
+				<th>F_Name</th>
+				<th>M_Init</th>
+				<th>L_Name</th>
+				<th>Phone_Num</th>
+				<th>Address</th>
+				<th>Rental</th>
+			</tr>
+
+			<?php
+
+			$query="SELECT * FROM Customer";
+			$result = mysqli_query($link, $query);
+				
+			while($Customer=mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+
+				echo "<td>".$Customer['Cust_Num']."</td>";
+
+				echo "<td>".$Customer['F_Name']."</td>";
+
+				echo "<td>".$Customer['M_Init']."</td>";
+
+				echo "<td>".$Customer['L_Name']."</td>";
+
+				echo "<td>".$Customer['Phone_Num']."</td>";
+
+				echo "<td>".$Customer['Address']."</td>";
+
+				echo "<td>".$Customer['Rental']."</td>";
+
+				echo "</tr>";
+			} //End While
+			?>
+		</table>
+
+		<br>
+
+		<table width="800" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>Plate_Num</th>
+				<th>Cust_Num</th>
+				<th>Color</th>
+				<th>Type</th>
+				<th>Make</th>
+				<th>VIN</th>
+				<th>Year</th>
+				<th>Mileage</th>
+			</tr>
+
+			<?php
+
+			$query4="SELECT * FROM vehicle";
+			$result4 = mysqli_query($link, $query4);
+				
+			while($Vehicle=mysqli_fetch_assoc($result4)) {
+				echo "<tr>";
+
+				echo "<td>".$Vehicle['Plate_Num']."</td>";
+
+				echo "<td>".$Vehicle['Cust_Num']."</td>";
+
+				echo "<td>".$Vehicle['Color']."</td>";
+
+				echo "<td>".$Vehicle['Type']."</td>";
+
+				echo "<td>".$Vehicle['Make']."</td>";
+
+				echo "<td>".$Vehicle['VIN']."</td>";
+
+				echo "<td>".$Vehicle['Year']."</td>";
+
+				echo "<td>".$Vehicle['Mileage']."</td>";
+
+				echo "</tr>";
+			} //End While
+			?>
+		</table>
+
+		<br>
+
+		<table width="300" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>ID_Num</th>
+				<th>Name</th>
+				<th>Phone_Num</th>
+			</tr>
+
+			<?php
+
+			$query1="SELECT * FROM insurance";
+			$result1 = mysqli_query($link, $query1);
+				
+			while($insurance=mysqli_fetch_assoc($result1)) {
+				echo "<tr>";
+
+				echo "<td>".$insurance['ID_Num']."</td>";
+
+				echo "<td>".$insurance['Name']."</td>";
+
+				echo "<td>".$insurance['Phone_Num']."</td>";
+
+				echo "</tr>";
+			} //End While
+			?>
+		</table>
+
+		<br>
+
+		<table width="300" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>ID_Num</th>
+				<th>Plate_Num</th>
+				<th>Claim_Num</th>
+			</tr>
+
+			<?php
+
+			$query2="SELECT * FROM insured_by";
+			$result2 = mysqli_query($link, $query2);
+				
+			while($insuredBy=mysqli_fetch_assoc($result2)) {
+				echo "<tr>";
+
+				echo "<td>".$insuredBy['ID_Num']."</td>";
+
+				echo "<td>".$insuredBy['Plate_Num']."</td>";
+
+				echo "<td>".$insuredBy['Claim_Num']."</td>";
+
+				echo "</tr>";
+			} //End While
+			?>
+		</table>
+
+
+		<br>
+
+		<table width="300" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>ID_Num</th>
+				<th>Cust_Num</th>
+				<th>Deductible</th>
+			</tr>
+
+			<?php
+
+			$query3="SELECT * FROM insured_with";
+			$result3 = mysqli_query($link, $query3);
+				
+			while($insuredWith=mysqli_fetch_assoc($result3)) {
+				echo "<tr>";
+
+				echo "<td>".$insuredWith['ID_Num']."</td>";
+
+				echo "<td>".$insuredWith['Cust_Num']."</td>";
+
+				echo "<td>".$insuredWith['Deductible']."</td>";
+
+				echo "</tr>";
+			} //End While
+			?>
+		</table>
+	</body>
+</html>
