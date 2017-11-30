e6f570fb42c2eac42c1d58a04e05891e4daeba29
JaskaranSidhu <32685059+JaskaranSidhu@users.noreply.github.com>
2017-11-30 13:30:32 -0700

    @@@ DONE @@@
    RO and WO in this table

--- View_Repair_Order.php
+++ View_Repair_Order.php
@@ -61,5 +61,30 @@ if(!$link) {
 			} //End While
 			?>
 		</table>
+
+		<br>
+
+		<table width="200" border="1" cellpadding="1" cellspacing="1">
+			<tr>
+				<th>RO_Num</th>
+				<th>ID</th>
+			</tr>
+
+			<?php
+
+			$query="SELECT * FROM works_on";
+			$result = mysqli_query($link, $query);
+				
+			while($WO=mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+
+				echo "<td>".$WO['RO_Num']."</td>";
+
+				echo "<td>".$WO['ID']."</td>";
+
+				echo "</tr>";
+			} //End While
+			?>
+		</table>
 	</body>
 </html>
