35519edba983d6f590c725b13e74c9ef3bae556a
Shannon TJ <shannontj@shaw.ca>
2017-11-30 20:10:48 -0700

    Search Finished

--- View_CustomerInfo.php
+++ View_CustomerInfo.php
@@ -19,12 +19,17 @@ if(!$link) {
                 <link rel="stylesheet" type="text/css" href="style.css">
         </head>
         <body>
+			
             <div class = "heading">
                 <a href="http://localhost/cpsc471/Home.php"><img src="https://d2oeydowngaei1.cloudfront.net/resources/front/images/carstar-logo.png"></a>
             </div>   
             <div class="tableview">
                 <table width="700" border="1" cellpadding="1" cellspacing="1">
-                    <h2> Customer Information </h2>    
+                    <h2> Customer Information </h2>  
+                    <form action ="View_CustomerInfo.php" method="GET">
+					<input type="text" name="search" placeholder="Enter a value"/>
+					<input type="submit" value="Search"/>
+					</form>  
                     <tr>
                                 <th>Cust_Num</th>
                                 <th>F_Name</th>
@@ -36,34 +41,102 @@ if(!$link) {
                         </tr>
 
                         <?php
+			//If the user performs a search
+			if(isset($_GET["search"]))
+			{
+				$search = $_GET["search"];
+				$search = mysqli_real_escape_string($link, $search);
+				
+				$query = "SELECT * FROM Customer WHERE Cust_Num = $search or Phone_Num = $search";
+				
+				$query2 = "SELECT * FROM Customer WHERE F_Name = '$search' or M_Init = '$search'
+				or L_Name = '$search' or Address = '$search' or Rental = '$search'";
+				
+				//If there are results...
+				if(mysqli_query($link, $query))
+				{
+					$result = mysqli_query($link, $query);	
 
-                        $query="SELECT * FROM Customer";
-                        $result = mysqli_query($link, $query);
+					while($Customer=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                        while($Customer=mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
+						echo "<td>".$Customer['Cust_Num']."</td>";
 
-                                echo "<td>".$Customer['Cust_Num']."</td>";
+						echo "<td>".$Customer['F_Name']."</td>";
 
-                                echo "<td>".$Customer['F_Name']."</td>";
+						echo "<td>".$Customer['M_Init']."</td>";
 
-                                echo "<td>".$Customer['M_Init']."</td>";
+						echo "<td>".$Customer['L_Name']."</td>";
 
-                                echo "<td>".$Customer['L_Name']."</td>";
+						echo "<td>".$Customer['Phone_Num']."</td>";
 
-                                echo "<td>".$Customer['Phone_Num']."</td>";
+						echo "<td>".$Customer['Address']."</td>";
 
-                                echo "<td>".$Customer['Address']."</td>";
+						echo "<td>".$Customer['Rental']."</td>";
 
-                                echo "<td>".$Customer['Rental']."</td>";
+						echo "</tr>";
+					} //End While					
+				}
+				else if(mysqli_query($link, $query2))
+				{
+					$result = mysqli_query($link, $query2);	
 
-                                echo "</tr>";
-                        } //End While
+					while($Customer=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
+
+						echo "<td>".$Customer['Cust_Num']."</td>";
+
+						echo "<td>".$Customer['F_Name']."</td>";
+
+						echo "<td>".$Customer['M_Init']."</td>";
+
+						echo "<td>".$Customer['L_Name']."</td>";
+
+						echo "<td>".$Customer['Phone_Num']."</td>";
+
+						echo "<td>".$Customer['Address']."</td>";
+
+						echo "<td>".$Customer['Rental']."</td>";
+
+						echo "</tr>";
+					} //End While					
+				}				
+			}
+			
+			else
+			{
+				$query="SELECT * FROM Customer";
+				$result = mysqli_query($link, $query);
+					
+				while($Customer=mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+
+					echo "<td>".$Customer['Cust_Num']."</td>";
+
+					echo "<td>".$Customer['F_Name']."</td>";
+
+					echo "<td>".$Customer['M_Init']."</td>";
+
+					echo "<td>".$Customer['L_Name']."</td>";
+
+					echo "<td>".$Customer['Phone_Num']."</td>";
+
+					echo "<td>".$Customer['Address']."</td>";
+
+					echo "<td>".$Customer['Rental']."</td>";
+
+					echo "</tr>";
+				} //End While
+			}
                         ?>
                 </table>
 
                 <br>
                 <h2> Vehicle Information </h2>
+                <form action ="View_CustomerInfo.php" method="GET">
+				<input type="text" name="search2" placeholder="Enter a value"/>
+				<input type="submit" value="Search"/>
+				</form>
                 <table width="800" border="1" cellpadding="1" cellspacing="1">
                         <tr>
                                 <th>Plate_Num</th>
@@ -78,35 +151,112 @@ if(!$link) {
 
                         <?php
 
-                        $query4="SELECT * FROM vehicle";
-                        $result4 = mysqli_query($link, $query4);
+			//If the user performs a search
+			if(isset($_GET["search2"]))
+			{
+				$search = $_GET["search2"];				
+				$search = mysqli_real_escape_string($link, $search);
+				
+				$query = "SELECT * FROM vehicle WHERE Cust_Num = $search or Year = $search or Mileage = $search";
+				
+				$query2 = "SELECT * FROM vehicle WHERE Plate_Num = '$search' or Color = '$search'
+				or Type = '$search' or Make = '$search' or VIN = '$search'";		
+				
+				//echo $query2;
+				
+				//If there are results...
+				if(mysqli_query($link, $query))
+				{
+					$result = mysqli_query($link, $query);	
+					
+					while($Vehicle=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                        while($Vehicle=mysqli_fetch_assoc($result4)) {
-                                echo "<tr>";
+						echo "<td>".$Vehicle['Plate_Num']."</td>";
 
-                                echo "<td>".$Vehicle['Plate_Num']."</td>";
+						echo "<td>".$Vehicle['Cust_Num']."</td>";
 
-                                echo "<td>".$Vehicle['Cust_Num']."</td>";
+						echo "<td>".$Vehicle['Color']."</td>";
 
-                                echo "<td>".$Vehicle['Color']."</td>";
+						echo "<td>".$Vehicle['Type']."</td>";
 
-                                echo "<td>".$Vehicle['Type']."</td>";
+						echo "<td>".$Vehicle['Make']."</td>";
 
-                                echo "<td>".$Vehicle['Make']."</td>";
+						echo "<td>".$Vehicle['VIN']."</td>";
 
-                                echo "<td>".$Vehicle['VIN']."</td>";
+						echo "<td>".$Vehicle['Year']."</td>";
 
-                                echo "<td>".$Vehicle['Year']."</td>";
+						echo "<td>".$Vehicle['Mileage']."</td>";
 
-                                echo "<td>".$Vehicle['Mileage']."</td>";
+						echo "</tr>";
+					} //End While					
+				}	
+				
+				else if(mysqli_query($link, $query2))
+				{
+					$result = mysqli_query($link, $query2);	
+					
+					while($Vehicle=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                                echo "</tr>";
-                        } //End While
+						echo "<td>".$Vehicle['Plate_Num']."</td>";
+
+						echo "<td>".$Vehicle['Cust_Num']."</td>";
+
+						echo "<td>".$Vehicle['Color']."</td>";
+
+						echo "<td>".$Vehicle['Type']."</td>";
+
+						echo "<td>".$Vehicle['Make']."</td>";
+
+						echo "<td>".$Vehicle['VIN']."</td>";
+
+						echo "<td>".$Vehicle['Year']."</td>";
+
+						echo "<td>".$Vehicle['Mileage']."</td>";
+
+						echo "</tr>";
+					} //End While					
+				}							
+						
+			}
+
+			else
+			{
+				$query4 = "SELECT * FROM vehicle";
+				$result4 = mysqli_query($link, $query4);
+					
+				while($Vehicle=mysqli_fetch_assoc($result4)) {
+					echo "<tr>";
+
+					echo "<td>".$Vehicle['Plate_Num']."</td>";
+
+					echo "<td>".$Vehicle['Cust_Num']."</td>";
+
+					echo "<td>".$Vehicle['Color']."</td>";
+
+					echo "<td>".$Vehicle['Type']."</td>";
+
+					echo "<td>".$Vehicle['Make']."</td>";
+
+					echo "<td>".$Vehicle['VIN']."</td>";
+
+					echo "<td>".$Vehicle['Year']."</td>";
+
+					echo "<td>".$Vehicle['Mileage']."</td>";
+
+					echo "</tr>";
+				} //End While
+			}
                         ?>
                 </table>
 
                 <br>
                 <h2> Insurance Information </h2>
+				<form action ="View_CustomerInfo.php" method="GET">
+				<input type="text" name="search3" placeholder="Enter a value"/>
+				<input type="submit" value="Search"/>
+				</form>               
                 <table width="300" border="1" cellpadding="1" cellspacing="1">
                         <tr>
                                 <th>ID_Num</th>
@@ -116,25 +266,78 @@ if(!$link) {
 
                         <?php
 
-                        $query1="SELECT * FROM insurance";
-                        $result1 = mysqli_query($link, $query1);
+ 			//If the user performs a search
+			if(isset($_GET["search3"]))
+			{
+				$search = $_GET["search3"];				
+				$search = mysqli_real_escape_string($link, $search);
+				
+				$query = "SELECT * FROM insurance WHERE ID_Num = $search or Phone_Num = $search";
+				
+				$query2 = "SELECT * FROM insurance WHERE Name = '$search'";
+				
+				//If there are results...
+				if(mysqli_query($link, $query))
+				{
+					$result = mysqli_query($link, $query);	
+					
+					while($insurance=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                        while($insurance=mysqli_fetch_assoc($result1)) {
-                                echo "<tr>";
+						echo "<td>".$insurance['ID_Num']."</td>";
 
-                                echo "<td>".$insurance['ID_Num']."</td>";
+						echo "<td>".$insurance['Name']."</td>";
 
-                                echo "<td>".$insurance['Name']."</td>";
+						echo "<td>".$insurance['Phone_Num']."</td>";
 
-                                echo "<td>".$insurance['Phone_Num']."</td>";
+						echo "</tr>";
+					} //End While					
+				}
+				
+				else if(mysqli_query($link, $query2))
+				{
+					$result = mysqli_query($link, $query2);		
+					
+					while($insurance=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                                echo "</tr>";
-                        } //End While
+						echo "<td>".$insurance['ID_Num']."</td>";
+
+						echo "<td>".$insurance['Name']."</td>";
+
+						echo "<td>".$insurance['Phone_Num']."</td>";
+
+						echo "</tr>";
+					} //End While								
+				}				
+			}
+
+			else
+			{
+				$query1="SELECT * FROM insurance";
+				$result1 = mysqli_query($link, $query1);
+					
+				while($insurance=mysqli_fetch_assoc($result1)) {
+					echo "<tr>";
+
+					echo "<td>".$insurance['ID_Num']."</td>";
+
+					echo "<td>".$insurance['Name']."</td>";
+
+					echo "<td>".$insurance['Phone_Num']."</td>";
+
+					echo "</tr>";
+				} //End While
+			}
                         ?>
                 </table>
 
                 <br>
                 <h2> Vehicle-Insurance Information </h2>
+				<form action ="View_CustomerInfo.php" method="GET">
+				<input type="text" name="search4" placeholder="Enter a value"/>
+				<input type="submit" value="Search"/>
+				</form>               
                 <table width="300" border="1" cellpadding="1" cellspacing="1">
                         <tr>
                                 <th>ID_Num</th>
@@ -144,24 +347,76 @@ if(!$link) {
 
                         <?php
 
-                        $query2="SELECT * FROM insured_by";
-                        $result2 = mysqli_query($link, $query2);
+		//If the user performs a search
+			if(isset($_GET["search4"]))
+			{
+				$search = $_GET["search4"];				
+				$search = mysqli_real_escape_string($link, $search);
+				
+				$query = "SELECT * FROM insured_by WHERE ID_Num = $search";
+				
+				$query2 = "SELECT * FROM insured_by WHERE Plate_Num = '$search' or Claim_Num = '$search'";	
+				
+				//If there are results...
+				if(mysqli_query($link, $query))
+				{
+					$result = mysqli_query($link, $query);	
 
-                        while($insuredBy=mysqli_fetch_assoc($result2)) {
-                                echo "<tr>";
+					while($insuredBy=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                                echo "<td>".$insuredBy['ID_Num']."</td>";
+						echo "<td>".$insuredBy['ID_Num']."</td>";
 
-                                echo "<td>".$insuredBy['Plate_Num']."</td>";
+						echo "<td>".$insuredBy['Plate_Num']."</td>";
 
-                                echo "<td>".$insuredBy['Claim_Num']."</td>";
+						echo "<td>".$insuredBy['Claim_Num']."</td>";
 
-                                echo "</tr>";
-                        } //End While
+						echo "</tr>";
+					} //End While					
+				}
+				else if(mysqli_query($link, $query2))
+				{
+					$result = mysqli_query($link, $query2);	
+					
+					while($insuredBy=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
+
+						echo "<td>".$insuredBy['ID_Num']."</td>";
+
+						echo "<td>".$insuredBy['Plate_Num']."</td>";
+
+						echo "<td>".$insuredBy['Claim_Num']."</td>";
+
+						echo "</tr>";
+					} //End While					
+				}											
+			}
+			
+			else
+			{
+				$query2="SELECT * FROM insured_by";
+				$result2 = mysqli_query($link, $query2);
+					
+				while($insuredBy=mysqli_fetch_assoc($result2)) {
+					echo "<tr>";
+
+					echo "<td>".$insuredBy['ID_Num']."</td>";
+
+					echo "<td>".$insuredBy['Plate_Num']."</td>";
+
+					echo "<td>".$insuredBy['Claim_Num']."</td>";
+
+					echo "</tr>";
+				} //End While
+			}
                         ?>
                 </table>
 
                 <h2> Customer-Insurance Information </h2>
+				<form action ="View_CustomerInfo.php" method="GET">
+				<input type="text" name="search5" placeholder="Enter a value"/>
+				<input type="submit" value="Search"/>
+				</form>               
                 <br>
 
                 <table width="300" border="1" cellpadding="1" cellspacing="1">
@@ -173,20 +428,50 @@ if(!$link) {
 
                         <?php
 
-                        $query3="SELECT * FROM insured_with";
-                        $result3 = mysqli_query($link, $query3);
+			//If the user performs a search
+			if(isset($_GET["search5"]))
+			{
+				$search = $_GET["search5"];				
+				$search = mysqli_real_escape_string($link, $search);
+				
+				$query = "SELECT * FROM insured_with WHERE ID_Num = $search or Cust_Num = $search or Deductible = $search";		
+				
+				//If there are results...
+				if(mysqli_query($link, $query))
+				{
+					$result = mysqli_query($link, $query);	
+				
+					while($insuredWith=mysqli_fetch_assoc($result)) {
+						echo "<tr>";
 
-                        while($insuredWith=mysqli_fetch_assoc($result3)) {
-                                echo "<tr>";
+						echo "<td>".$insuredWith['ID_Num']."</td>";
 
-                                echo "<td>".$insuredWith['ID_Num']."</td>";
+						echo "<td>".$insuredWith['Cust_Num']."</td>";
 
-                                echo "<td>".$insuredWith['Cust_Num']."</td>";
+						echo "<td>".$insuredWith['Deductible']."</td>";
 
-                                echo "<td>".$insuredWith['Deductible']."</td>";
+						echo "</tr>";
+					} //End While				
+				}
+			}
+			
+			else
+			{
+				$query3="SELECT * FROM insured_with";
+				$result3 = mysqli_query($link, $query3);
+					
+				while($insuredWith=mysqli_fetch_assoc($result3)) {
+					echo "<tr>";
 
-                                echo "</tr>";
-                        } //End While
+					echo "<td>".$insuredWith['ID_Num']."</td>";
+
+					echo "<td>".$insuredWith['Cust_Num']."</td>";
+
+					echo "<td>".$insuredWith['Deductible']."</td>";
+
+					echo "</tr>";
+				} //End While
+			}
                         ?>
                 </table>
             </div>
